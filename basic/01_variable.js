c5d008c4e969d5775ce2587ceaa22e52da9505a2
yanghayong <hayong5621@daum.net>
2023-01-03 14:33:13 +0900

    javascript

--- basic/01_variable.js
+++ basic/01_variable.js
@@ -1,13 +1,13 @@
-// variable 변수
+// Variable 변수
 // var, let, const
-// scope -> 블럭기반 {...}
+// scope -> 블럭 {...}
 
 // 1. var
-// 읽기,쓰기 가능하다
-// 블럭을 무시한다
+// 읽기, 쓰기 가능하다.
+// 블럭을 무시한다.
 
 {
-  var user = "john"
+  var user = "John"
   user = "Alice"
   console.log(user)
 }
@@ -15,7 +15,7 @@
 console.log(user)
 
 // 2. let
-// 읽기와 쓰기가 가능하다.
+// 읽기, 쓰기가 가능하다.
 
 {
   let title = "html"
@@ -28,37 +28,42 @@ console.log(user)
 // console.log(title)
 
 // 3. const
-// 상수 - 읽기만 가능. 변하지 않는다
+// 상수 - 읽기만 가능. 변하지 않는다.
 
 {
   const subject = "야인시대"
   console.log(subject)
-  //   subject = "베트맨"
+  // subject = '배트맨'
 }
 
-// 4. 변수의 데이터 타입
-// 원시데이터(primitive)
-// number, String, Boolean, null, undefined
-// Object : 싱글데이터를 묶어놓은 데이터
-
+// 4. 변수의 데이타 타입
+// 원시데이타(primitive) : Number, String, Boolean, null, undefined
+// object : 싱글데이타를 묶어놓은 데이타
 // const myClass = {
-//     subject: "html"
-//     date: "2023-01-15"
+//     subject: 'html',
+//     date: '2022-12-13',
 // }
-
 // function
 // const hahaha = function () {
-//   // 할일을 적어준다
-//   console.log("hahaha")
+//     console.log('hahaha~~~~~')
 // }
 
-// number
+// Number
 const num = 100
 const width = 10.5
 console.log(`값은 ${num}이고, 타입은 ${typeof num}입니다.`)
 
-const firstName = "john"
-const lastName = "snow"
+const a = 1 / 0
+const b = -25 / 0
+const c = "html" / 10
 
-// console.log(firstName+``+lastName) 옛방식
-// console.log(`${firstName} ${lastName}`) es6방식
+console.log(a)
+console.log(b)
+console.log(c)
+
+// string
+const firstname = "Jonh"
+const lastName = "snow"
+const fullName = firstname + lastName
+console.log(fullName)
+console.log(`${firstname} ${lastName}`)
---
+++ basic/02_operator.js
@@ -0,0 +1,92 @@
+// 1. 문자열 연결 연산자
+// +
+console.log("10") // 문자열
+console.log(10) // 숫자
+console.log(typeof ("10" + 2))
+
+// 2. 산술 연산자
+console.log(100 + 10)
+console.log(100 - 10)
+console.log(100 * 10)
+console.log(100 ** 10)
+console.log(100 / 10)
+console.log(100 % 10)
+console.log(10 % 2)
+
+// 3. 증감 연산자
+let number = 8
+console.log(number)
+number++
+console.log(number)
+number--
+console.log(number)
+
+let num1 = 10
+let num2 = ++num1
+console.log(num1, num2)
+
+let count1 = 10
+let count2 = count1++
+console.log(count1, count2)
+
+// 4. 할당연산자
+let x = 10
+let y = 5
+
+x = x + y // x += y
+console.log(x)
+
+x = x * y // x *= y
+console.log(x)
+
+x = x / y // x /= y
+console.log(x)
+
+x = x % y // x %= y
+console.log(x)
+
+// 5. 비교 연산자
+console.log(1000 < 100)
+console.log(1000 <= 100) // =<, =>
+console.log(1000 > 100)
+console.log(1000 >= 100)
+
+// 6. 논리 연산자
+
+// ||(or), &&(and), !(not)
+
+let a = 100 > 50 // true
+let b = 10 < 5 // false
+let c = true // true
+
+// || => 하나라도 true 이면 -> true
+console.log(a || b || c)
+
+// && => 전부 true 일 때만 -> true
+console.log(a && b && c)
+
+// ! -> 값을 반대로 바꾼다.
+b = !b
+console.log(b)
+
+// 7. 동등 / 일치 연산자
+// =
+// ==
+// ===
+const value1 = "100"
+const value2 = 100
+
+console.log(value1 == value2)
+console.log(value1 === value2)
+
+console.clear()
+console.log("\\\\\\\\\\\\")
+console.log(0 == false)
+console.log(1 == true)
+console.log(0 === false)
+console.log(1 === true)
+console.log("" == false)
+console.log("" === false)
+console.log(null == undefined)
+console.log(null === undefined)
+console.log("\\\\\\\\\\\\")
