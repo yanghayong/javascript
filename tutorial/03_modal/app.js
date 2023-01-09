bbd219b651664fda03c7d118465667b3b38a637f
yanghayong <hayong5621@daum.net>
2023-01-09 15:37:16 +0900

    javascript

--- tutorial/03_modal/app.js
+++ tutorial/03_modal/app.js
@@ -4,3 +4,8 @@ const modal = document.querySelector(".modal")
 const close = document.querySelector(".close")
 
 console.log(btn, dimm, modal, close)
+
+btn.addEventListener("click", function () {
+  dimm.classList.add("open")
+  modal.classList.add("open")
+})
---
+++ tutorial/04_sticky_nav/app.js
@@ -0,0 +1,5 @@
+window.addEventListener("scroll", function () {
+  console.log(window.scrollY)
+  const menu = document.querySelector(".inner")
+  menu.classList.toggle("sticky", window.scrollY > 300)
+})
