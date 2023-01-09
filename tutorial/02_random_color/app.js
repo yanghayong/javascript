37c9d31bfd74cb94e0bd8693f645ef4a1fc0c59c
yanghayong <hayong5621@daum.net>
2023-01-09 14:33:15 +0900

    javascript

---
+++ tutorial/02_random_color/app.js
@@ -0,0 +1,37 @@
+const bg = ["orange", "blue", "red", "yellow", "green"]
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"]
+const color = document.getElementById("color")
+const bgBtn = document.getElementById("bgBtn")
+const hexBtn = document.getElementById("hexBtn")
+
+console.log(bg, hex, color, bgBtn, hexBtn)
+
+function getBgNum() {
+  return Math.floor(Math.random() * bg.length)
+}
+
+function getHexNum() {
+  return Math.floor(Math.random() * hex.length)
+}
+
+function changeColor() {
+  const randomBgNum = getBgNum()
+  document.body.style.backgroundColor = bg[randomBgNum]
+  color.textContent = bg[randomBgNum]
+}
+
+function changeHex() {
+  let hexColor = "#"
+  for (let i = 0; i < 6; i++) {
+    const j = getHexNum()
+    hexColor = hexColor + hex[getHexNum()]
+  }
+
+  document.body.style.backgroundColor = hexColor
+  color.textContent = hexColor
+}
+
+bgBtn.addEventListener("click", changeColor)
+hexBtn.addEventListener("click", changeHex)
+
+// setInterval(changeHex, 50)
---
+++ tutorial/03_modal/app.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".btn")
+const dimm = document.querySelector(".dimm")
+const modal = document.querySelector(".modal")
+const close = document.querySelector(".close")
+
+console.log(btn, dimm, modal, close)
