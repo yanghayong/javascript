1110b3aa72cccb1c70bd8b9049fb2ea4f06d46d9
yanghayong <hayong5621@daum.net>
2023-01-11 16:21:28 +0900

    javascript

--- tutorial/08_email/app.js
+++ tutorial/08_email/app.js
@@ -1,18 +1,33 @@
+const dimm = document.querySelector(".dimm")
+const modal = document.querySelector(".modal")
+const closeBtn = document.querySelector(".close")
+const inputs = document.querySelectorAll("input")
+console.log(inputs)
+
 window.onload = function () {
   document
-    .getElementById('contact-form')
-    .addEventListener('submit', function (event) {
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
       event.preventDefault()
+      dimm.classList.add("active")
       // generate a five digit number for the contact_number variable
       this.contact_number.value = (Math.random() * 100000) | 0
       // these IDs from the previous steps
-      emailjs.sendForm('service_ibur23b', 'template_cpzziah', this).then(
+      emailjs.sendForm("service_ibur23b", "template_cpzziah", this).then(
         function () {
-          console.log('SUCCESS!')
+          console.log("SUCCESS!")
+          modal.classList.add("active")
         },
         function (error) {
-          console.log('FAILED...', error)
+          console.log("FAILED...", error)
         }
       )
     })
+
+  closeBtn.addEventListener("click", function () {
+    modal.classList.remove("active")
+    dimm.classList.remove("active")
+
+    inputs.forEach((item) => (item.value = ""))
+  })
 }
