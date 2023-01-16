c78a80a4aa6aa8fe81a6a32dd376d9faa1730627
yanghayong <hayong5621@daum.net>
2023-01-16 17:00:28 +0900

    javascript

--- tutorial/11_todo/js/weather.js
+++ tutorial/11_todo/js/weather.js
@@ -1,9 +1,27 @@
 const weather = document.querySelector('.weather')
+const API_KEY = '26a8f813a56d73684d04cdff5a1f7720'
+
+async function getWeather(lat, lon) {
+  console.log(lat, lon)
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+
+  const response = await fetch(url)
+  const data = await response.json()
+  console.log(data)
+}
 
 function success(position) {
   const lat = position.coords.latitude
   const lon = position.coords.longitude
-  weather.innerText = `${lat}, ${lon}`
+
+  const coords = {
+    latitude: lat,
+    longitude: lon,
+  }
+
+  localStorage.setItem('coords', JSON.stringify(coords))
+  getWeather(lat, lon)
 }
 
 function error() {
